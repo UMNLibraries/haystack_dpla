37baaf73c4627204dbd4d7edf2f87248c3924394
Chad Fennell <libsys@gmail.com>
2019-07-23 12:42:08 -0500

    make the matches dir configurable

--- run.sh
+++ run.sh
@@ -3,8 +3,11 @@
 # Get a fresh copy of DPLA data
 node run_downloader.js;
 
+MATCHES_DIR=$0
+
+
 for filename in ./dpla_data/*.gz; do
     [ -e "$filename" ] || continue
-    echo "nohup node run -i $filename &";
-    eval "nohup node run -i $filename &"
+    echo "nohup node run -i $filename -m $MATCHES_DIR &";
+    eval "nohup node run -i $filename -m $MATCHES_DIR &"
 done
